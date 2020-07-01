7c7db50ef595cb14d177792cc8b12ffb43cf9364
kesavanand007 <66354090+kesavanand007@users.noreply.github.com>
2020-07-01 12:08:05 +0530

    ASSIGNMENT4

---
+++ JERRY.js
@@ -0,0 +1,11 @@
+Yaakovgreeter.sayHello();
+Johngreeter.sayGoodBye();
+Jengreeter.sayGoodBye();
+Jasongreeter.sayGoodBye();
+Paulgreeter.sayHello();
+Frankgreeter.sayHello();
+Larrygreeter.sayHello();
+Paulagreeter.sayHello();
+Lauragreeter.sayHello();
+Jimgreeter.sayGoodBye();
+
---
+++ tom.js
@@ -0,0 +1,119 @@
+(function (window) {
+  var Yaakovgreeter={};
+  Yaakovgreeter.name="Yaakov";
+  var greeting=" Hello ";
+  Yaakovgreeter.sayHello= function () {
+	console.log(greeting +Yaakovgreeter.name);
+  }
+
+  window.Yaakovgreeter=Yaakovgreeter;
+
+})(window);
+
+(function (window) {
+  var Johngreeter={};
+  Johngreeter.name="John";
+  var greeting=" Good Bye ";
+  Johngreeter.sayGoodBye= function () {
+	console.log(greeting +Johngreeter.name);
+  }
+
+  window.Johngreeter=Johngreeter;
+
+})(window);
+
+(function (window) {
+  var Jengreeter={};
+  Jengreeter.name="Jen";
+  var greeting=" Good Bye ";
+  Jengreeter.sayGoodBye= function () {
+	console.log(greeting +Jengreeter.name);
+  }
+
+  window.Jengreeter=Jengreeter;
+
+})(window);
+
+(function (window) {
+  var Jasongreeter={};
+  Jasongreeter.name="Jason";
+  var greeting=" Good Bye ";
+  Jasongreeter.sayGoodBye= function () {
+	console.log(greeting +Jasongreeter.name);
+  }
+
+  window.Jasongreeter=Jasongreeter;
+
+})(window);
+
+(function (window) {
+  var Paulgreeter={};
+  Paulgreeter.name="Paul";
+  var greeting=" Hello ";
+  Paulgreeter.sayHello= function () {
+	console.log(greeting +Paulgreeter.name);
+  }
+
+  window.Paulgreeter=Paulgreeter;
+
+})(window);
+
+(function (window) {
+  var Frankgreeter={};
+  Frankgreeter.name="Frank";
+  var greeting=" Hello ";
+  Frankgreeter.sayHello= function () {
+	console.log(greeting +Frankgreeter.name);
+  }
+
+  window.Frankgreeter=Frankgreeter;
+
+})(window);
+
+(function (window) {
+  var Larrygreeter={};
+  Larrygreeter.name="Larry";
+  var greeting=" Hello ";
+  Larrygreeter.sayHello= function () {
+	console.log(greeting +Larrygreeter.name);
+  }
+
+  window.Larrygreeter=Larrygreeter;
+
+})(window);
+
+(function (window) {
+  var Paulagreeter={};
+  Paulagreeter.name="Paula";
+  var greeting=" Hello ";
+  Paulagreeter.sayHello= function () {
+	console.log(greeting +Paulagreeter.name);
+  }
+
+  window.Paulagreeter=Paulagreeter;
+
+})(window);
+
+(function (window) {
+  var Lauragreeter={};
+  Lauragreeter.name="Laura";
+  var greeting=" Hello ";
+  Lauragreeter.sayHello= function () {
+	console.log(greeting +Lauragreeter.name);
+  }
+
+  window.Lauragreeter=Lauragreeter;
+
+})(window);
+
+(function (window) {
+  var Jimgreeter={};
+  Jimgreeter.name="Jim";
+  var greeting=" Good Bye ";
+  Jimgreeter.sayGoodBye= function () {
+	console.log(greeting +Jimgreeter.name);
+  }
+
+  window.Jimgreeter=Jimgreeter;
+
+})(window);
